300bd6a66540bff646d214266ce8a9ae414d17f4
mchen <mfchen@outlook.com>
2022-02-16 21:09:52 -0500

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,16 +1,20 @@
+/*
+ * Jonathan Corbet, Alessandro Rubini, and Greg Kroah-Hartman. 2005. 
+ * <i>Linux Device Drivers, 3rd Edition</i>. O'Reilly Media, Inc.
+ */
 #include <linux/init.h>
 #include <linux/module.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-  printk(KERN_ALERT "Hello, world\n");
-  return 0;
+    printk(KERN_ALERT "Hello, world\n");
+    return 0;
 }
 
 static void hello_exit(void)
 {
-  printk(KERN_ALERT "Goodbye, cruel world\n");
+    printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 
 module_init(hello_init);
